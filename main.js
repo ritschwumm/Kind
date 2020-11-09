79189010dd1ef53414e91011327ea98433c6c5b3
maiavictor <srvictormaia@gmail.com>
2020-11-09 09:48:13 -0300

    Fix initial patch path

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var fs = require("fs");
 var {execSync} = require("child_process");
 
 // Builds fm.js
-//execSync(`rm fm.js; fmjs main | js-beautify >> fm.js`);
+execSync(`rm fm.js; fmjs main | js-beautify >> fm.js`);
 
 // Runs main.fmfm using fm.js
 var fmjs = fs.readFileSync("./fm.js", "utf8");
