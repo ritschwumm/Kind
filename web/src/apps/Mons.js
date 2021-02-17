94fe615da73e462e045f3309934327e6d7fa0768
Maisa <maisa.milena@gmail.com>
2021-02-17 20:07:47 -0300

    Error building Mons.js

--- web/src/apps/Mons.js
+++ web/src/apps/Mons.js
@@ -1,12 +1,2 @@
 Compilation error.
-RangeError: Maximum call stack size exceeded
-at Fm$escape$(/Users/maisa / .npm - global / lib / node_modules / formality - js / src / formality.js: 21229: 24)
-at Fm$escape$(/Users/maisa / .npm - global / lib / node_modules / formality - js / src / formality.js: 21238: 27)
-at Fm$escape$(/Users/maisa / .npm - global / lib / node_modules / formality - js / src / formality.js: 21238: 27)
-at Fm$escape$(/Users/maisa / .npm - global / lib / node_modules / formality - js / src / formality.js: 21238: 27)
-at Fm$escape$(/Users/maisa / .npm - global / lib / node_modules / formality - js / src / formality.js: 21238: 27)
-at Fm$escape$(/Users/maisa / .npm - global / lib / node_modules / formality - js / src / formality.js: 21238: 27)
-at Fm$escape$(/Users/maisa / .npm - global / lib / node_modules / formality - js / src / formality.js: 21238: 27)
-at Fm$escape$(/Users/maisa / .npm - global / lib / node_modules / formality - js / src / formality.js: 21238: 27)
-at Fm$escape$(/Users/maisa / .npm - global / lib / node_modules / formality - js / src / formality.js: 21238: 27)
-at Fm$escape$(/Users/maisa / .npm - global / lib / node_modules / formality - js / src / formality.js: 21238: 27)
+Unexpected escape char: '\5'.
